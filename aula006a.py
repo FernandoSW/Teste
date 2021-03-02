df4692cbed75d8fdea9c8032772a0aaffe9b4b16
FernandoSW <79880304+FernandoSW@users.noreply.github.com>
2021-03-01 21:09:30 -0300

    Criação dos diretórios
    Aula

---
+++ aula006a.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+
+# print('A soma entre', n1, 'e', n2, 'vale:', s)
+# print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+# Ou coloque um f antes das aspas " " e escreva o nome da variavel dentro dos colchetes {}  exemplo:
+
+print(f'A soma de {n1} e {n2} é {s}')
+
+
